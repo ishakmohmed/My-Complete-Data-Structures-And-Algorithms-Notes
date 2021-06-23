2cce7bcb89db9d30eed2f311dffe3c66ee5716b3
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-06-23 16:12:38 +0800

    Update MY ATTEMPT AT SOLVING OZI'S DSA QUESTION (it works).java

--- MY ATTEMPT AT SOLVING OZI'S DSA QUESTION (it works).java
+++ MY ATTEMPT AT SOLVING OZI'S DSA QUESTION (it works).java
@@ -7,9 +7,8 @@ public class Main {
     public static int displayBase10(String baseTwoNumber) {
       int[] array = new int[baseTwoNumber.length()];
 
-      for (int i = 0; i < baseTwoNumber.length() - 1; i++) {
+      for (int i = 0; i < baseTwoNumber.length() - 1; i++)
         array[i] = (int)(baseTwoNumber.charAt(i) - 48);
-      }
 
       int total = 0;
       int index = 0;
